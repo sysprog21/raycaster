83008e280dfea88786e0c8130ac810ace2a04228
alanjian85 <alanjian85@outlook.com>
2022-07-18 11:18:42 +0800

    Initialize .BSS section
    All static variables are required to be initialized to 0,
    during the start of C runtime

--- main.c
+++ main.c
@@ -9,14 +9,13 @@
 
 void kmain()
 {
-    mem_init();
-    uart_init();
     uart_puts("Hello world!\n");
     uint32_t *fb = fb_create(SCREEN_WIDTH, SCREEN_HEIGHT, 32);
     RayCaster *rayCaster = RayCasterFixedConstruct();
     Game game = GameConstruct();
     Renderer renderer = RendererConstruct(rayCaster);
     RendererTraceFrame(&renderer, &game, fb);
+    rayCaster->Destruct(rayCaster);
     while (1) {
         uart_putc(uart_getc());
         uart_putc('\n');
--- raycaster.h
+++ raycaster.h
@@ -8,8 +8,6 @@
 #define SCREEN_WIDTH (uint16_t) 320
 #define SCREEN_HEIGHT (uint16_t) 256
 #define SCREEN_SCALE 2
-#define FOV (double) (M_PI / 2)
-#define INV_FACTOR (float) (SCREEN_WIDTH * 95.0f / 320.0f)
 #define LOOKUP_TBL
 #define LOOKUP8(tbl, offset) tbl[offset]
 #define LOOKUP16(tbl, offset) tbl[offset]
@@ -19,7 +17,7 @@
 #define MAP_Y (uint8_t) 32
 #define INV_FACTOR_INT ((uint16_t) (SCREEN_WIDTH * 75))
 #define MIN_DIST (int) ((150 * ((float) SCREEN_WIDTH / (float) SCREEN_HEIGHT)))
-#define HORIZON_HEIGHT (SCREEN_HEIGHT / 2)
+#define HORIZON_HEIGHT (SCREEN_HEIGHT >> 1)
 #define INVERT(x) (uint8_t)((x ^ 255) + 1)
 #define UMULT(x, y) (uint16_t)(((uint32_t) (x) * (uint32_t) (y)) >> 8)
 #define ABS(x) (x < 0 ? -x : x)
